38ad849e2edff5acda5604d2de10c4bcfb27d004
sebas <sebastianrp21@hotmail.com>
2023-08-23 08:21:13 -0500

    update 23/08/2023

--- app/Http/Controllers/Firebase/ContactController.php
+++ app/Http/Controllers/Firebase/ContactController.php
@@ -4,11 +4,39 @@ namespace App\Http\Controllers\Firebase;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Database;
+
 
 class ContactController extends Controller
 {
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+        $this->tablename = 'contacts';
+    }
+
     public function index()
     {
         return view('firebase.contact.index');
+    }
+
+    public function create()
+    {
+        return view('firebase.contact.create');
+    }
+    public function store(Request $request)
+    {
+        $postData = [
+            'fname' => $request->first_name,
+            'lname' => $request->last_name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+        ];
+        $postRef = $this->database->getReference($this->tablename)->push($postData);
+        if ($postRef) {
+            return redirect('contacts')->with('status', 'Contact Added Successfully');
+        } else {
+            return redirect('contacts')->with('status', 'Contact Not Added');
+        }
     }
 }
--- resources/views/firebase/contact/index.blade.php
+++ resources/views/firebase/contact/index.blade.php
@@ -5,10 +5,15 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+
+                @if (session('status'))
+                    <h4 class="alert alert-warning mb-2">{{ session('status') }}</h4>
+                @endif
+
                 <div class="card">
                     <div class="card-header">
                         <h4>Contact List
-                            <a href="{{ url('add - contact') }}" class="btn btn-sm btn-primary float-end">Add contact</a>
+                            <a href="{{ url('add-contact') }}" class="btn btn-sm btn-primary float-end">Add contact</a>
                         </h4>
                     </div>
                     <div class="card-body">
